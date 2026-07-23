209917a73e6aaef0852ed784083b30c4716562b7
viktorygis <gis07072016@gmail.com>
2026-07-23 14:36:34 +0300

    Правки в техниках - оптимизация

---
+++ assets/serf-session-BEuzb3YC.js
@@ -0,0 +1,1 @@
+import"./main-BkAgXcap.js";import{t as e}from"./session-animations-Zn8jDS4Q.js";document.addEventListener(`DOMContentLoaded`,function(){let t=new e({type:`simple`});t.init(),window.addEventListener(`beforeunload`,()=>t.destroy())});
